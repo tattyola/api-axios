4682d4e29e163ae689dd9b6fe1d0d3206a7436a6
Tatsiana A <astrouskaya@yahoo.com>
2022-10-23 16:19:06 -0400

    second commit

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,23 +1,39 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import postAuth from './authService';
-// import { config } from "dotenv";
-// config();
 
 let auth;
-let data = {
+let validData = {
     email: process.env.EMAIL,
     password: process.env.PASSWORD
 };
+let invalidData = {
+    email: 'invalidCredentials',
+    password: 'invalidPassword'
+};
 
-describe('auth', function ()  {
+describe('auth', function () {
 
-    before(async () => {
-        auth = await postAuth.authRequest(data);
-        await console.log(`Auth    ${auth}`)
-    })
+    describe('success auth', function () {
 
-    it('success log in', function () {
-        expect(auth.status).to.equal(200);
-        expect(auth.statusText).to.equal('OK');
-    })
+        before(async () => {
+            auth = await postAuth.authRequest(validData);
+        });
+
+        it('success log in', function () {
+            expect(auth.status).to.equal(200);
+            expect(auth.statusText).to.equal('OK');
+        });
+    });
+
+    describe('fail auth', function () {
+
+        before(async () => {
+            auth = await postAuth.authRequest(invalidData);
+            await console.log(`Auth    ${auth.statusText}`)
+        });
+
+        it('fail log in', function () {
+            expect(auth.message).to.equal('Request failed with status code 401');
+        });
+    });
 })
